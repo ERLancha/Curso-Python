59e27b57002fa6983c86601f9e775e7fa912d715
ERLancha <122731476+ERLancha@users.noreply.github.com>
2023-01-21 14:38:15 +0100

    Filtros

--- PANDAS_PROJECT/filtros.py
+++ PANDAS_PROJECT/filtros.py
@@ -5,4 +5,23 @@ df = pd.read_csv('../Datos/survey-2022/data.csv',index_col='ResponseId')
 schema_df = pd.read_csv('../Datos/survey-2022/data_schema.csv')
 
 #print(df.columns)
-print(df[['Currency', 'CompTotal']])
+#print(df[['Currency', 'CompTotal']])
+
+
+#Creamos filtro
+
+#Sueldos mayores de 45000 (en moneda local), que trabajen con Python.
+#na=False es para controlar los errores
+#filt = (df['CompTotal']> 45000 & df['LanguageHaveWorkedWith'].str.contains('Python',na=False))
+
+#aplicamos filtro
+#print(df.loc[filt, ['Country','LanguageHaveWorkedWith', 'CompTotal']])
+
+countries = ["Spain","India"]
+filtroPaises = df["Country"].isin(countries)
+
+#Sueldos mayores de 45000, que trabajen con Python y que sean de España e India
+filt = ((df['CompTotal']> 45000) & (df['LanguageHaveWorkedWith'].str.contains('Python',na=False)) & filtroPaises)
+
+#aplicamos filtro
+print(df.loc[filt, ['Country','LanguageHaveWorkedWith', 'CompTotal']])
